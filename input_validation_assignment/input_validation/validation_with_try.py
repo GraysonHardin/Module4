600e16ecc95aa78a107845556763b4007ee52afe
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-16 10:02:46 -0500

    Refactored code and established a proper first test.

--- input_validation_assignment/input_validation/validation_with_try.py
+++ input_validation_assignment/input_validation/validation_with_try.py
@@ -1,27 +1,14 @@
 """
-Program: average_scores.py
+Program: validation_with_try.py
 Author: Grayson Hardin
-Last date modified: 9/10/2020
+Last date modified: 9/16/2020
 """
 
 
-def average():
-    # Each variable down below (score1 - score3) will collect user input.
-    score1 = int(input("Enter first number here:"))
-    score2 = int(input("Enter second number here:"))
-    score3 = int(input("Enter third number here:"))
-    average_scores = (score1 + score2 + score3) / 3  # Here the we perform calculation on the previous variables.
-    return average_scores
-
-
-def main():
-    # Like the previous, we collect another set of variables (last name, first name, and age)
-    last_name = input("Enter last name:")
-    first_name = input("Enter first name:")
-    age = input("Enter age:")
-    average_score = average()
-    print(f'{last_name}, {first_name} age: {age} average grade: {average_score}') # return the values and format accordingly.
+def average(score1, score2, score3):
+    NUMBER_TESTS = 3
+    return float((score1 + score2 + score3) / NUMBER_TESTS)
 
 
 if __name__ == '__main__':
-    main()
+    average()
--- input_validation_assignment/test/test_validation_with_try.py
+++ input_validation_assignment/test/test_validation_with_try.py
@@ -1,13 +1,9 @@
 import unittest
-from unittest import mock
+
 from input_validation_assignment.input_validation.validation_with_try import average
 
 
 class MyTestCase(unittest.TestCase):
-    def test_average(self):
-        with mock.patch('builtins.input', side_effect=[85, 90, 95]):
-            assert average() == 90
-
     def test_average_exception(self):
         with self.assertRaises(ValueError):
             average(-90, 89, 78)
