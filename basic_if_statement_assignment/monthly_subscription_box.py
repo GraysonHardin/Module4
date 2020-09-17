0c1265fcfef6f32b3587a814cac7034067f7ba1c
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-17 13:32:52 -0500

    Completed the subscription service assignment. Returns expected code and includes mock testing.

--- basic_if_statement_assignment/monthly_subscription_box.py
+++ basic_if_statement_assignment/monthly_subscription_box.py
@@ -1,7 +1,7 @@
 """
 Program: monthly_subscription.py
 Author: Grayson Hardin
-Last date modified:
+Last date modified: 9/17/2020
 
 This program allows the user to select a membership type. Upon selecting, it will return the version they selected, the cost, and the benefits of the version.
 As a bonus, I added an Else-Statement to handle incorrect inputs and it will force the user to enter a valid input.
@@ -9,28 +9,31 @@ As a bonus, I added an Else-Statement to handle incorrect inputs and it will for
 """
 
 
-def membership():
-    print("Please choose your monthly Programmer's Toolkit Subscription Service: Platinum, Gold, Silver, Bronze, and Free Trial. ")
-    user_input = str(input())
+def membership(user_input):
     if user_input == 'Platinum':
-        print("You have selected the Platinum which costs $60")
+        return f'You have selected the Platinum which costs $60'
 
     elif user_input == 'Gold':
-        print('You have selected the Gold which costs $50')
+        return f'You have selected the Gold which costs $50'
 
     elif user_input == 'Silver':
-        print('You have selected the Silver which costs $40')
+        return f'You have selected the Silver which costs $40'
 
     elif user_input == 'Bronze':
-        print('You have selected the Bronze which costs $30')
+        return f'You have selected the Bronze which costs $30'
 
     elif user_input == 'Free Trial':
-        print('You have selected the Free Trial which is free!')
+        return f'You have selected the Free Trial which is free!'
 
     else:
-        print('Error, please input a valid option. Try again.')
-        return membership()
+        return f'You have entered an invalid value.'
+
+
+def main():
+    print("Please choose your monthly Programmer's Toolkit Subscription Service: Platinum, Gold, Silver, Bronze, and Free Trial. ")
+    user_input = str(input("Enter serivce choice here: "))
+    print(membership(user_input))
 
 
 if __name__ == '__main__':
-    membership()
+    main()
--- basic_if_statement_assignment/test_monthly_subscription_box.py
+++ basic_if_statement_assignment/test_monthly_subscription_box.py
@@ -1,42 +1,46 @@
 import unittest
 from unittest.mock import patch
-
-from basic_if_statement_assignment.monthly_subscription_box import membership
+from basic_if_statement_assignment.monthly_subscription_box import main
 
 
 @patch('builtins.input')
 @patch('builtins.print')
 class MyTestCase(unittest.TestCase):
-
     def test_membership_platinum(self, mock_print, mock_input):
         mock_input.return_value = 'Platinum'
 
-        membership()
+        main()
         mock_print.assert_called_with('You have selected the Platinum which costs $60')
 
     def test_membership_gold(self, mock_print, mock_input):
         mock_input.return_value = 'Gold'
 
-        membership()
+        main()
         mock_print.assert_called_with('You have selected the Gold which costs $50')
 
     def test_membership_silver(self, mock_print, mock_input):
         mock_input.return_value = 'Silver'
 
-        membership()
+        main()
         mock_print.assert_called_with('You have selected the Silver which costs $40')
 
     def test_membership_bronze(self, mock_print, mock_input):
         mock_input.return_value = 'Bronze'
 
-        membership()
+        main()
         mock_print.assert_called_with('You have selected the Bronze which costs $30')
 
     def test_membership_free_trial(self, mock_print, mock_input):
         mock_input.return_value = 'Free Trial'
 
-        membership()
+        main()
         mock_print.assert_called_with('You have selected the Free Trial which is free!')
+
+    def test_false_input(self, mock_print, mock_input):
+        mock_input.return_value = 'Invalid'
+
+        main()
+        mock_print.assert_called_with('You have entered an invalid value.')
 
 
 if __name__ == '__main__':
