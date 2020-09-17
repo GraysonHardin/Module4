6aea1cd7aa7acab50e5d8750c087642af4eded64
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-17 13:15:31 -0500

    Copied code from previous assignment (to calculate average of positive values). 3 of 4 tests pass.

--- input_validation_assignment/test/test_validation_with_try.py
+++ input_validation_assignment/test/test_validation_with_try.py
@@ -3,6 +3,10 @@ from input_validation_assignment.input_validation.validation_with_try import ave
 
 
 class MyTestCase(unittest.TestCase):
+    def test_average(self):
+        actual = average(90, 85, 95)
+        expected = 90
+        self.assertEqual(expected, actual)
     def test_average_first_number_negative(self):
         with self.assertRaises(ValueError):
             average(90, 85, 95)
