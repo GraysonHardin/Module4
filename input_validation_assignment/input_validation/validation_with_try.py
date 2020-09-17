13dc5ffdee5e0b229bf9dfc9cca5c2751179f3a6
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-17 12:10:08 -0500

    Added Value Error, test passes.

--- input_validation_assignment/input_validation/validation_with_try.py
+++ input_validation_assignment/input_validation/validation_with_try.py
@@ -7,7 +7,10 @@ Last date modified: 9/17/2020
 
 def average(score1, score2, score3):
     NUMBER_TESTS = 3
+    if score1 < 0 or score2 < 0 or score3 < 0:
+        raise ValueError
     return float((score1 + score2 + score3) / NUMBER_TESTS)
+
 
 
 if __name__ == '__main__':
