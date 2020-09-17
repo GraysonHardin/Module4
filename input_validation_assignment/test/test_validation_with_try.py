8b445e929841b386c7a621f52051af6c9ccc4e0f
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-17 13:10:18 -0500

    Added second test (failed)

--- input_validation_assignment/test/test_validation_with_try.py
+++ input_validation_assignment/test/test_validation_with_try.py
@@ -5,8 +5,11 @@ from input_validation_assignment.input_validation.validation_with_try import ave
 class MyTestCase(unittest.TestCase):
     def test_average_first_number_negative(self):
         with self.assertRaises(ValueError):
-            average(-90, 85, 95)
+            average(90, 85, 95)
 
+    def test_average_second_number_negative(self):
+        with self.assertRaises(ValueError):
+            average(90, -85, 95)
 
 if __name__ == '__main__':
     unittest.main()
