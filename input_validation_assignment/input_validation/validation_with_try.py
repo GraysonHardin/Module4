4e09e35c677dadac95b671660598dd27f174d65c
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-17 12:06:08 -0500

    Test failed.

--- input_validation_assignment/input_validation/validation_with_try.py
+++ input_validation_assignment/input_validation/validation_with_try.py
@@ -1,16 +1,12 @@
 """
 Program: validation_with_try.py
 Author: Grayson Hardin
-Last date modified: 9/16/2020
+Last date modified: 9/17/2020
 """
 
 
 def average(score1, score2, score3):
     NUMBER_TESTS = 3
-    if score1 < 0:
-        raise ValueError
-    elif score2 < 0:
-        raise ValueError
     return float((score1 + score2 + score3) / NUMBER_TESTS)
 
 
--- input_validation_assignment/test/test_validation_with_try.py
+++ input_validation_assignment/test/test_validation_with_try.py
@@ -1,19 +1,13 @@
 import unittest
-
 from input_validation_assignment.input_validation.validation_with_try import average
 
 
 class MyTestCase(unittest.TestCase):
     def test_average_first_number_negative(self):
         with self.assertRaises(ValueError):
-            average(-90, 89, 78)
-
-    def test_average_second_number_negative(self):
-        with self.assertRaises(ValueError):
-            average(90, -89, 78)
-
-
+            average(-90, 85, 95)
 
 
 if __name__ == '__main__':
     unittest.main()
+
