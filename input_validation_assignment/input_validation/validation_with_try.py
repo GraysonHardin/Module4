795cd018f38c4f3fc7afd4097b027fd24f5941b1
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-17 13:23:35 -0500

    Copied main from old assignment and added the try / except argument. Code functions as intended.

--- input_validation_assignment/input_validation/validation_with_try.py
+++ input_validation_assignment/input_validation/validation_with_try.py
@@ -2,6 +2,10 @@
 Program: validation_with_try.py
 Author: Grayson Hardin
 Last date modified: 9/17/2020
+
+
+This program (like last week's assignment) will collect user's name (first, last) and age, along with three scores.
+This time however, the program will return a "Invalid input" message if a user enters a negative value (i.e; -95).
 """
 
 
@@ -12,6 +16,19 @@ def average(score1, score2, score3):
     return float((score1 + score2 + score3) / NUMBER_TESTS)
 
 
+def main():
+    last_name = input("Enter last name:")
+    first_name = input("Enter first name:")
+    age = input("Enter age:")
+    score1 = int(input("Enter first number: "))
+    score2 = int(input("Enter second number: "))
+    score3 = int(input("Enter third number: "))
+    try:
+        average_score = average(score1, score2, score3)
+        print(f'{last_name}, {first_name} age: {age} average grade: {average_score}')
+    except:
+        print('Invalid input. Please try again later.')
+
 
 if __name__ == '__main__':
-    average()
+    main()
--- input_validation_assignment/test/test_validation_with_try.py
+++ input_validation_assignment/test/test_validation_with_try.py
@@ -7,9 +7,10 @@ class MyTestCase(unittest.TestCase):
         actual = average(90, 85, 95)
         expected = 90
         self.assertEqual(expected, actual)
+
     def test_average_first_number_negative(self):
         with self.assertRaises(ValueError):
-            average(90, 85, 95)
+            average(-90, 85, 95)
 
     def test_average_second_number_negative(self):
         with self.assertRaises(ValueError):
